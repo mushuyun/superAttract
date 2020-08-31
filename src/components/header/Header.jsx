e0d80cc9fa485645b1cade20eaf2eb400c37b992
Sue Mu <mushuyun@yahoo.com>
2020-08-31 16:24:31 -0400

    set up header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import { ReactComponent as Logo } from '../../assets/crown.svg';
-
 import './Header.scss';
 
 const Header = () => (
@@ -16,6 +14,9 @@ const Header = () => (
       </Link>
       <Link className='option' to='/shop'>
         CONTACT
+      </Link>
+      <Link className='option' to='/signin'>
+        SIGN IN
       </Link>
     </div>
   </div>
